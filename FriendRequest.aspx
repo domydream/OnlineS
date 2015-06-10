859efdf24d3214bb37d346df5102a769f41f9950
domydream <thai.mepro@gmail.com>
2015-06-10 13:24:34 +0700

    hehe
    new hot :))

--- FriendRequest.aspx
+++ FriendRequest.aspx
@@ -1,7 +1,23 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MyMasterPage.master" AutoEventWireup="true" CodeFile="FriendRequest.aspx.cs" Inherits="FriendRequest" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+          
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+    <asp:TextBox ID="findFriend" runat="server"  CssClass="form-control"></asp:TextBox>
+    <cc1:AutoCompleteExtender ID="AutoCompleteExtender1"
+        EnableCaching="true"
+        MinimumPrefixLength="1"
+        ServiceMethod="ListUser"
+        CompletionSetCount="1"      
+        UseContextKey="true"
+        CompletionInterval="1000"
+        TargetControlID="findFriend"
+        runat="server" FirstRowSelected="false">
+    </cc1:AutoCompleteExtender>
 </asp:Content>
 
--- FriendRequest.aspx.cs
+++ FriendRequest.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,60 @@ public partial class FriendRequest : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        ((MyMasterPage)Master).active = "inbox";
+        if (Session["user"] == null && Request.Cookies["user"] == null)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+        else
+        {
+            if (Session["user"] == null && Request.Cookies["user"] != null)
+            {
+                Session["user"] = new UserModel().getByUserName(Request.Cookies["user"].Value);
+            }
+            ((MyMasterPage)Master).u = (User)Session["user"];                                                                       
+        }
+    }
+      [System.Web.Script.Services.ScriptMethod()]      
+      [System.Web.Services.WebMethod ]
+    public static List<string> ListUser(string prefixText)
+
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["OnlineSMS"].ToString());
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from [user] where phone like @Name+'%' or username like @Name+'%'", con);
+
+            cmd.Parameters.AddWithValue("@Name", prefixText);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+        //    BaseConnect sql = new BaseConnect(ConfigurationManager.ConnectionStrings["OnlineSMS"].ConnectionString);
+        //    DataTable dt = sql.excuteQuery("SELECT * FROM [user] WHERE phone LIKE " + prefixText, null);
+            //DataTable dt = new UserModel().getUser();
+
+            List<string> Names = new List<string>();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Names.Add(dt.Rows[i]["username"].ToString());
+            }
+
+            return Names;
+        }
+        catch (Exception)
+        {
+
+            return null;
+        }
+       
 
     }
+
 }
